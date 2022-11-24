8dc2be204ecaeeb7753bda9bffdc6de3063a4566
King Lemming <kinglemming@gmail.com>
2022-11-23 19:51:18 -0800

    Update ToolsComplement.java

--- src/main/java/cofh/toolscomplement/ToolsComplement.java
+++ src/main/java/cofh/toolscomplement/ToolsComplement.java
@@ -18,6 +18,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import static cofh.lib.util.constants.ModIds.ID_TOOLS_COMPLEMENT;
+import static cofh.toolscomplement.init.TComFlags.*;
 
 @Mod (ID_TOOLS_COMPLEMENT)
 public class ToolsComplement {
@@ -45,6 +46,32 @@ public class ToolsComplement {
         TComConfig.register();
 
         TComItems.register();
+
+        // TODO: Temporary; waiting on new config system.
+        setFlag(FLAG_IRON_TOOLS, true);
+        setFlag(FLAG_GOLD_TOOLS, true);
+        setFlag(FLAG_DIAMOND_TOOLS, true);
+        setFlag(FLAG_NETHERITE_TOOLS, true);
+
+        setFlag(FLAG_COPPER_TOOLS, true);
+        setFlag(FLAG_TIN_TOOLS, true);
+        setFlag(FLAG_LEAD_TOOLS, true);
+        setFlag(FLAG_SILVER_TOOLS, true);
+        setFlag(FLAG_NICKEL_TOOLS, true);
+        setFlag(FLAG_BRONZE_TOOLS, true);
+        setFlag(FLAG_ELECTRUM_TOOLS, true);
+        setFlag(FLAG_INVAR_TOOLS, true);
+        setFlag(FLAG_CONSTANTAN_TOOLS, true);
+
+        setFlag(FLAG_COPPER_ARMOR, true);
+        setFlag(FLAG_TIN_ARMOR, true);
+        setFlag(FLAG_LEAD_ARMOR, true);
+        setFlag(FLAG_SILVER_ARMOR, true);
+        setFlag(FLAG_NICKEL_ARMOR, true);
+        setFlag(FLAG_BRONZE_ARMOR, true);
+        setFlag(FLAG_ELECTRUM_ARMOR, true);
+        setFlag(FLAG_INVAR_ARMOR, true);
+        setFlag(FLAG_CONSTANTAN_ARMOR, true);
     }
 
     // region INITIALIZATION
